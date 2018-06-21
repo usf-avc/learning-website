169455fa857074762ebc8efe6463f54171921293
Ian Sanders <iansan5653@gmail.com>
2018-06-21 12:42:30 -0400

    Fix transition for category collapsing

--- collapse-categories.js
+++ collapse-categories.js
@@ -10,9 +10,24 @@ function toggleSectionCollapse(collapseButton) {
 }
 
 /**
+ * Add the height of each section as a style directly to the element.
+ */
+function updateSectionHeights() {
+  let sectionElements = document.querySelectorAll(".category"),
+      sectionElementsArray = Array.from(sectionElements);
+
+  sectionElementsArray.forEach(function(sectionElement) {
+    // If we don't reset the height, it will always be the same thing that it
+    // was first set to.
+    sectionElement.style.height = null;
+    sectionElement.style.height = `${sectionElement.offsetHeight}px`;
+  });
+}
+
+/**
  * Add click listeners to all the collapse buttons on the page.
  */
-function addCollapseButtonEventListeners() {
+function initializeCategoryCollapsing() {
   // Find all of the buttons on the page:
   let collapseButtonElements = document.querySelectorAll(".collapseCategoryButton");
 
@@ -21,11 +36,23 @@ function addCollapseButtonEventListeners() {
 
   // Loop through the array, assigning the event listener to each button:
   collapseButtonElementsArray.forEach(function(buttonElement) {
-    console.log(buttonElement);
     buttonElement.addEventListener("click", function() {
       toggleSectionCollapse(this);
     });
   });
 
+  // This is almost perfect, but there's a small problem. Even though there
+  // is a height transition set in CSS, the collapse isn't going to animate
+  // because you have to set an original height. But if a height is set in CSS,
+  // some content might get cut out when the section isn't collapsed. So instead,
+  // we'll use JavaScript to get all of the uncollapsed heights and then set 
+  // them in CSS.
+  updateSectionHeights();
 
+  // Much better! Except...what if the page gets resized and the element height
+  // changes because the text reflowed? Let's listen for page resizing in case
+  // that happens.
+  window.addEventListener("resize", function() {
+    updateSectionHeights();
+  });
 }
